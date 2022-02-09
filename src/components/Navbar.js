12a01d7b6db920e8e4d81c98a66823040d29eb59
Abhay <leader.abhay002@gmail.com>
2022-02-09 23:30:20 +0530

    Removed Searchbar from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,85 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar(props) {
-    return (
-        <div>
-           <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextPro</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-        <li className={`nav-item  text-${props.mode==="light"?"dark":"light"}`}>
-          <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
-        </li>
-        <li className={`nav-item  text-${props.mode==="light"?"dark":"light"}`}>
-          <a className="nav-link" href="/About">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Contact Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Coming Soon...</a>
-        </li>
-      </ul>
-      <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}`}> {/* we have used ternary operator */}
-      <input className="form-check-input" onClick={props.changemode} type="checkbox" id="flexSwitchCheckDefault"/>
-      <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Darkmode</label>
-       </div>
-     <form className="d-flex mx-2">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-    
-  </div>
-</nav>
+  return (
+    <div>
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            TextPro
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="/navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li
+                className={`nav-item  text-${
+                  props.mode === "light" ? "dark" : "light"
+                }`}
+              >
+                <a className="nav-link active" aria-current="page" href="/">
+                  {props.home}
+                </a>
+              </li>
+              <li
+                className={`nav-item  text-${
+                  props.mode === "light" ? "dark" : "light"
+                }`}
+              >
+                <a className="nav-link" href="/About">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Contact Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link disabled"
+                  href="/"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Coming Soon...
+                </a>
+              </li>
+            </ul>
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              {" "}
+              {/* we have used ternary operator */}
+              <input
+                className="form-check-input"
+                onClick={props.changemode}
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+              />
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckDefault"
+              >
+                Enable Darkmode
+              </label>
+            </div>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
